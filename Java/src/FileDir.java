a435cae7a5c1df61637b28933b0975750a1a5e6d
Salatik <Chernetsov2008@gmail.com>
2026-04-27 15:29:50 +0300

    27.04 TryCatch and File lessons

--- Java/src/FileDir.java
+++ Java/src/FileDir.java
@@ -1,24 +1,19 @@
-// Code do nothing. Need to rewrite
+//Simple code. Displays files and directories in a path
 import java.io.File;
 
-class FileDir {
     void main() {
         mkDir();
     }
     void mkDir() {
-        File dir1 = new File("Users/user/projects/", "test.txt");
-        boolean created = dir1.mkdir();
-        if(created) {
-            IO.println("Directory has been created");
-        }
-        String JavaDir = "Users/user/projects/JavaDir";
-        File file1 = new File(JavaDir, "Test.txt");
-        if(dir1.isDirectory()) {
-            for(File item : dir1.listFiles()) {
-                if(item.isFile()) {
-                    IO.println("It's file!");
+        File dir = new File("/Users/user/projects");
+        if(dir.isDirectory()) {
+            for(File item : dir.listFiles()) {
+                if(item.isDirectory()) {
+                    IO.println(item.getName() + " in " + item.getParent() + " is a directory");
+                }
+                else {
+                    IO.println(item.getName() + " in " + item.getParent() + " is a file");
                 }
             }
         }
     }
-}
