2b16e31196d6397aa4605a23d1a01c3677093db9
Salatik <Chernetsov2008@gmail.com>
2026-04-27 10:37:26 +0300

    TryCatch

---
+++ Java/src/TryCatch.java
@@ -0,0 +1,24 @@
+class TryCatch {
+    public static void main() {
+        divide(10, 0);
+        divide(12,4);
+        IO.println("Done.");
+    }
+
+    static void divide(int a, int b) {
+        String errorMassage = null;
+
+        try {
+            int result = a / b;
+            IO.println(result);
+        }
+        catch(Throwable e) {
+            IO.println("Exception!");
+            errorMassage = e.getMessage();
+        }
+        finally {
+            IO.println("Finally");
+            IO.println(errorMassage);
+        }
+    }
+}
